6c240815cd7d3c43f6cd952d524914a94393b6d7
johnBuffer <jean.tampon@Mediane.local>
2017-03-02 14:40:14 +0100

    color test

--- wrappers/python/frame_convert2.py
+++ wrappers/python/frame_convert2.py
@@ -3,29 +3,30 @@ import numpy as np
 def get_gradient_color(value, max_value=1024.0):
     #we use 4 colours in our gradient so we normalize value in [0, 4]
     normalized_value = float(float(value)*4.0/max_value)
-    gradient_color = (0, 0, 0)
+    (r, g, b) = (0, 0, 0)
 
     #print(normalized_value)
 
     #yellow -> red
     if normalized_value > 3:
         normalized_value -= 3
-        gradient_color = (255, 255*(1-normalized_value), 0)
+        (r, g, b) = (255, 255*(1-normalized_value), 0)
     #green -> yellow
     elif normalized_value > 2:
         normalized_value -= 2
-        gradient_color = (255*normalized_value, 255, 0)
+        (r, g, b) = (255*normalized_value, 255, 0)
     #cyan -> green
     elif normalized_value > 1:
         normalized_value -= 2
-        gradient_color = (0, 255, 255*(1-normalized_value))
+        (r, g, b) = (0, 255, 255*(1-normalized_value))
     #blue -> cyan
     else:
-        gradient_color = (0, 255*(1-normalized_value), 255)
-
-    (r, g, b) = gradient_color
+        (r, g, b) = (0, 255*(1-normalized_value), 255)
 
     return (b, r, g)
+
+def apply_gradient(matrix):
+    map(get_gradient_color, matrix)
 
 
 def pretty_depth(depth):
@@ -46,11 +47,10 @@ def pretty_depth(depth):
     depth = depth.astype(np.uint8)
     """
 
-    gradient_image = [[]]*len(depth)
-    for l in range(len(depth)):
-        gradient_image[l] = map(get_gradient_color, depth[l])
+    gradient_image = map(apply_gradient, depth)
+    
 
-    depth = np.array(gradient_image)
+    #depth = np.array(gradient_image)
     depth = depth.astype(np.uint8)
 
     return depth
