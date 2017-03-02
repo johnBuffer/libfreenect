1a11f84671126f64495c8c6b78c1f39bc09d8e1b
johnBuffer <jean.tampon@Mediane.local>
2017-03-02 15:08:03 +0100

    color test

--- wrappers/python/frame_convert2.py
+++ wrappers/python/frame_convert2.py
@@ -3,7 +3,6 @@ import numpy as np
 def get_gradient_color(value, max_value=1024.0):
     #we use 4 colours in our gradient so we normalize value in [0, 4]
     normalized_value = float(float(value)*4.0/max_value)
-    normalized_value = (normalized_value-2)*2
     (r, g, b) = (0, 0, 0)
 
     #print(normalized_value)
