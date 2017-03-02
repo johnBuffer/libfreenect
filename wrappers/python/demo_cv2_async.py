ee5fcd1c6df735ce159a431f0e6e8dc0033e9116
johnBuffer <jean.tampon@Mediane.local>
2017-03-02 17:11:43 +0100

    color test

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -34,10 +34,10 @@ def body(*args):
 
 
 print('Press ESC in window to stop')
-'''freenect.runloop(depth=display_depth,
+freenect.runloop(depth=display_depth,
                  video=None,
-                 body=body)'''
-
-freenect.runloop(depth=None,
-                 video=display_rgb,
                  body=body)
+
+'''freenect.runloop(depth=None,
+                 video=display_rgb,
+                 body=body)'''
