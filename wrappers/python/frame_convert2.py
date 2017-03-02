b880acd8b79c26ab6582656d736f12607dc6ceb4
johnBuffer <jean.tampon@Mediane.local>
2017-03-02 13:11:55 +0100

    changed depth display

--- wrappers/python/frame_convert2.py
+++ wrappers/python/frame_convert2.py
@@ -1,5 +1,29 @@
 import numpy as np
 
+def get_gradient_color(value, max_value=1024):
+    #we use 4 colours in our gradient so we normalize value in [0, 4]
+    normalized_value = value*4/max_value
+
+    gradient_color = (0, 0, 0)
+
+    #yellow -> red
+    if normalized_value > 3:
+        normalized_value -= 3
+        gradient_color = (255, 255*(1-normalized_value), 0)
+    #green -> yellow
+    elif normalized_value > 2:
+        normalized_value -= 2
+        gradient_color = (255*normalized_value, 255, 0)
+    #cyan -> green
+    elif normalized_value > 1:
+        normalized_value -= 2
+        gradient_color = (0, 255, 255*(1-normalized_value))
+    #blue -> cyan
+    else:
+        gradient_color = (0, 255*(1-normalized_value), 255)
+
+    return gradient_color
+
 
 def pretty_depth(depth):
     """Converts depth into a 'nicer' format for display
@@ -12,13 +36,13 @@ def pretty_depth(depth):
     Returns:
         A numpy array that has been processed with unspecified datatype
     """
-    np.clip(depth, 0, 1023, depth)
-    depth >>= 2
-    depth = depth.astype(np.uint8)
 
-    color_depth = np.zeros((len(depth), len(depth[0]), 3))
+    vector_gradient_color = np.vectorize(get_gradient_color)
 
-    return color_depth
+    np.clip(depth, 0, 1024, depth)
+    #depth = depth.astype(np.uint8) 
+
+    return vector_gradient_color(depth)
 
 
 def pretty_depth_cv(depth):
