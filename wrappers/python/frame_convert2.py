e5c14b5706713fad05ea4f0fe1179121f17d722c
johnBuffer <jean.tampon@Mediane.local>
2017-03-02 11:49:10 +0100

    changed depth display

--- wrappers/python/frame_convert2.py
+++ wrappers/python/frame_convert2.py
@@ -12,7 +12,7 @@ def pretty_depth(depth):
     Returns:
         A numpy array that has been processed with unspecified datatype
     """
-    np.clip(depth, 0, 512, depth)
+    np.clip(depth, 0, 1023, depth)
     depth >>= 2
     depth = depth.astype(np.uint8)
     return depth
