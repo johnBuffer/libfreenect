839ae7f05335dd7e442021abc0d10dc1e354714a
johnBuffer <jean.tampon@Mediane.local>
2017-03-03 09:21:06 +0100

    canny

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -12,7 +12,7 @@ keep_running = True
 def display_depth(dev, data, timestamp):
     global keep_running
     #data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
-    data2 = applyColorMap(data, cv2.COLORMAP_JET)
+    data2 = cv2.applyColorMap(data, cv2.COLORMAP_JET)
     cv2.imshow('Depth', data2)
     if cv2.waitKey(10) == 27:
         keep_running = False
