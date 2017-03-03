596e58c0f6cb6adce7a6d5d8101905cd221865a5
johnBuffer <jean.tampon@Mediane.local>
2017-03-03 10:07:43 +0100

    added img for tests

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -15,7 +15,7 @@ def find_object(data, object):
     img2 = object # trainImage
 
     # Initiate SIFT detector
-    sift = cv2.SIFT()
+    sift = cv2.xfeatures2d.SIFT_create()
 
     # find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(img1, None)
