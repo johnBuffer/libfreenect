23816a4f360baec569d458fb07b95566b8f17154
johnBuffer <jean.tampon@Mediane.local>
2017-03-03 09:19:57 +0100

    canny

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -12,8 +12,8 @@ keep_running = True
 def display_depth(dev, data, timestamp):
     global keep_running
     #data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
-    data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
-    cv2.imshow('Depth', cv2.resize(data2, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST))
+    data2 = applyColorMap(data, cv2.COLORMAP_JET)
+    cv2.imshow('Depth', data2)
     if cv2.waitKey(10) == 27:
         keep_running = False
 
@@ -39,10 +39,10 @@ def body(*args):
 
 
 print('Press ESC in window to stop')
-"""freenect.runloop(depth=display_depth,
+freenect.runloop(depth=display_depth,
                  video=None,
-                 body=body)"""
-
-freenect.runloop(depth=None,
-                 video=display_rgb,
                  body=body)
+
+"""freenect.runloop(depth=None,
+                 video=display_rgb,
+                 body=body)"""
