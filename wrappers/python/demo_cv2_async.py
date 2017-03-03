967b77e3b533c9f063f5a03e1f835baed38443a2
johnBuffer <jean.tampon@Mediane.local>
2017-03-03 10:05:31 +0100

    added img for tests

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -8,6 +8,67 @@ import numpy as np
 cv2.namedWindow('RGB')
 keep_running = True
 
+def find_object(data, object):
+    MIN_MATCH_COUNT = 10
+
+    img1 = data # queryImage
+    img2 = object # trainImage
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m, n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h,w = img1.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        matchesMask = None
+
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+    img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+
+    return img3
+
+def draw_canny(data):
+    gray = frame_convert2.video_cv(data)
+    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+
+    return edged
+
 
 def display_depth(dev, data, timestamp):
     global keep_running
@@ -21,15 +82,11 @@ def display_depth(dev, data, timestamp):
 
 def display_rgb(dev, data, timestamp):
     global keep_running
-    gray = frame_convert2.video_cv(data)
-    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    edged = cv2.Canny(gray, 35, 125)
+    
+    calendar = cv2.imread("media/img.jpg", 0)
+    result = find_object(data, calendar)
 
-    """box = np.int0(cv2.cv.BoxPoints(marker))
-    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)"""
-
-    cv2.imshow('RGB', edged)
+    cv2.imshow('RGB', result)
     if cv2.waitKey(10) == 27:
         keep_running = False
 
@@ -40,10 +97,10 @@ def body(*args):
 
 
 print('Press ESC in window to stop')
-freenect.runloop(depth=display_depth,
+"""freenect.runloop(depth=display_depth,
                  video=None,
-                 body=body)
-
-"""freenect.runloop(depth=None,
-                 video=display_rgb,
                  body=body)"""
+
+freenect.runloop(depth=None,
+                 video=display_rgb,
+                 body=body)
