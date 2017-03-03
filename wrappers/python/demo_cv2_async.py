8d73b5a7e3a17382dbd7c84fc9fbca6a4c169b22
johnBuffer <jean.tampon@Mediane.local>
2017-03-03 09:16:45 +0100

    canny

--- wrappers/python/demo_cv2_async.py
+++ wrappers/python/demo_cv2_async.py
@@ -11,6 +11,7 @@ keep_running = True
 
 def display_depth(dev, data, timestamp):
     global keep_running
+    #data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
     data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
     cv2.imshow('Depth', cv2.resize(data2, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST))
     if cv2.waitKey(10) == 27:
@@ -24,8 +25,8 @@ def display_rgb(dev, data, timestamp):
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 35, 125)
 
-    box = np.int0(cv2.cv.BoxPoints(marker))
-    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+    """box = np.int0(cv2.cv.BoxPoints(marker))
+    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)"""
 
     cv2.imshow('RGB', edged)
     if cv2.waitKey(10) == 27:
